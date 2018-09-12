7ea95431b230e5bcdcefacc4d5173e24df5ecca9
Ali Muhammed <uniapi@outlook.com>
2018-09-12 19:23:41 +0300

    Added [src/index.js] - the implementation of celstofahr

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*************************************************************************
+                          Written by Ali Muhammed
+                            uniapi@outlook.com
+                            September 12, 2018
+*************************************************************************/
+
+function celstofahr(temp) {
+    return temp * 9 / 5 + 32;
+};
+
+export { celstofahr };
